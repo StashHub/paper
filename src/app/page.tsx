0bfa161687cbd4d8d4c16556ecba430524e25485
Damian Gaynor <drgayno@gmail.com>
2024-04-11 16:19:09 -0400

    feat: add clerk auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from 'next/image';
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
-  return <h1 className='text-2xl'>Hello</h1>;
+  return (
+    <div className='h-screen'>
+      <UserButton />
+    </div>
+  );
 }
