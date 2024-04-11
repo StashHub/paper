3e539feb4f1155fd2651e3be1ab467229305a6ee
Damian Gaynor <drgayno@gmail.com>
2024-04-11 17:56:42 -0400

    feat: add clerk auth pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,43 @@
-import { UserButton } from '@clerk/nextjs';
+import { Icons } from '@/components/icons';
+import { Button } from '@/components/ui/button';
+import { UserButton, auth } from '@clerk/nextjs';
+import Link from 'next/link';
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = auth();
+  const isAuth = !!userId;
+
   return (
-    <div className='h-screen'>
-      <UserButton />
+    <div className='w-screen min-h-screen bg-gradient-to-br from-rose-100 to-teal-100'>
+      <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+        <div className='flex flex-col items-center text-center'>
+          <div className='flex items-center'>
+            <h1 className='mr-3 text-5xl font-semibold'>Chat with any PDF</h1>
+            <UserButton afterSignOutUrl='/' />
+          </div>
+
+          <div className='flex mt-2'>
+            {isAuth && <Button>Go to Chats</Button>}
+          </div>
+
+          <p className='max-w-xl mt-2 text-lg text-slate-500'>
+            Join millions of students, researchers and professionals to
+            instantly answer questions and understand research with AI
+          </p>
+
+          <div className='w-full mt-6'>
+            {isAuth ? (
+              <h1>FileUpload</h1>
+            ) : (
+              <Link href='/auth/sign-in'>
+                <Button>
+                  Get Started <Icons.chevronRight className='w-4 h-4 ml-2' />
+                </Button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
