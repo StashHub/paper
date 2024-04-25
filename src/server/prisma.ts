1ca1281d8e212bd89a282326452e1aaa9111992c
Damian Gaynor <drgayno@gmail.com>
2024-04-25 15:38:44 -0400

    feat: add prisma extensions

--- src/server/prisma.ts
+++ src/server/prisma.ts
@@ -3,19 +3,48 @@
  * @link https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices
  */
 import { env } from '@/env';
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 const prismaGlobal = global as typeof global & {
   prisma?: PrismaClient;
 };
 
-export const prisma: PrismaClient =
+export const db: PrismaClient =
   prismaGlobal.prisma ??
   new PrismaClient({
     log:
       env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
   });
 
-if (env.NODE_ENV !== 'production') {
-  prismaGlobal.prisma = prisma;
-}
+if (env.NODE_ENV !== 'production') prismaGlobal.prisma = db;
+
+// -- Extensions --
+
+export const prisma = db
+  .$extends({
+    client: { $log: (s: string) => console.log(s) },
+  })
+  .$extends({
+    model: {
+      $allModels: {
+        // `exists` method available on all models
+        async exists<T>(
+          this: T,
+          where: Prisma.Args<T, 'findFirst'>['where']
+        ): Promise<boolean> {
+          // Get the current model at runtime
+          const context = Prisma.getExtensionContext(this);
+          const result = await (context as any).findFirst({ where });
+          return result !== null;
+        },
+        // `getById` method available on all models
+        async getById<T>(
+          this: T,
+          id: string
+        ): Promise<Prisma.Result<T, { id: string }, 'findUniqueOrThrow'>> {
+          const context = Prisma.getExtensionContext(this);
+          return await (context as any).findUniqueOrThrow({ where: { id } });
+        },
+      },
+    },
+  });
--- src/server/routers/user.ts
+++ src/server/routers/user.ts
@@ -2,8 +2,6 @@ import { router, protectedProcedure } from '@/server/trpc';
 
 export const userRouter = router({
   byId: protectedProcedure.query(async ({ ctx }) => {
-    return ctx.prisma.user.findFirst({
-      where: { id: ctx.session.user?.id },
-    });
+    return ctx.prisma.user.getById(ctx.session.user?.id);
   }),
 });
