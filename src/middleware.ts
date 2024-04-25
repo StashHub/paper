69f8e4481059102a0acd8c022ba148206d25cc48
Damian Gaynor <drgayno@gmail.com>
2024-04-25 19:42:51 -0400

    feat: add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { withAuth } from '@kinde-oss/kinde-auth-nextjs/middleware';
+import type { KindeUser } from '@kinde-oss/kinde-auth-nextjs/types';
+
+type NextRequestWithAuth = Request & {
+  kindeAuth: { user: KindeUser; token: string };
+};
+
+export default function middleware(req: NextRequestWithAuth) {
+  return withAuth(req);
+}
+
+export const config = {
+  matcher: ['/dashboard/:path*'],
+};
