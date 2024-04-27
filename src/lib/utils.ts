d0454ec39e9665b0dd6ea4d19bea05e7647e7552
Damian Gaynor <drgayno@gmail.com>
2024-04-27 00:36:53 -0400

    feat: add additional trpc endpoints

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function absolute(path: string) {
+  if (typeof window !== 'undefined') return path; // Browser should use relative URL
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}${path}`; // SSR should use Vercel URL
+  return `http://localhost:${process.env.PORT ?? 3000}${path}`; // Dev SSR should use localhost
 }
--- src/server/routers/file.ts
+++ src/server/routers/file.ts
@@ -1,5 +1,6 @@
 import { router, authProcedure } from '@/server/trpc';
 import { Status } from '@prisma/client';
+import { TRPCError } from '@trpc/server';
 import { z } from 'zod';
 
 const FileStatus: [Status, ...Status[]] = Object.values(Status) as [
@@ -8,6 +9,11 @@ const FileStatus: [Status, ...Status[]] = Object.values(Status) as [
 ];
 
 export const fileRouter = router({
+  list: authProcedure.query(async ({ ctx }) => {
+    return await ctx.prisma.file.findMany({
+      where: { userId: ctx.session.user.id },
+    });
+  }),
   exist: authProcedure
     .input(z.object({ key: z.string() }))
     .query(async ({ input, ctx }) => {
@@ -28,6 +34,22 @@ export const fileRouter = router({
         data: input,
         select: { id: true },
       });
+      return file;
+    }),
+  delete: authProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ input, ctx }) => {
+      const file = await ctx.prisma.file.findUnique({
+        where: { id: input.id, userId: ctx.session.user.id },
+      });
+      if (!file) {
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+          message: `No file with id '${input.id}'`,
+        });
+      }
+
+      await ctx.prisma.file.delete({ where: { id: input.id } });
       return file;
     }),
   update: authProcedure
