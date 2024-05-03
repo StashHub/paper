2b9ac6325a106e728383d9c3a65b43fec4075faf
Damian Gaynor <drgayno@gmail.com>
2024-05-02 22:37:52 -0400

    refactor: modify message route

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,127 +1,118 @@
 import { prisma } from '@/server/prisma';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
-import { NextApiRequest, NextApiResponse } from 'next';
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { NextRequest, NextResponse } from 'next/server';
+import { OpenAIEmbeddings } from '@langchain/openai';
 import { PineconeStore } from '@langchain/pinecone';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
 
 import { env } from '@/env';
 import { pinecone } from '@/lib/pinecone';
 import { openai } from '@/lib/openai';
+import { z } from 'zod';
+import { api } from '@/trpc/server';
+import type { CreateMessageInput } from '@/types/trpc';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const { method } = req;
+const schema = z.object({
+  fileId: z.string(),
+  message: z.string(),
+});
 
-  switch (method) {
-    case 'POST':
-      const { fileId, message } = req.body as {
-        fileId: string;
-        message: string;
-      };
+export const POST = async (req: NextRequest) => {
+  // Get the user object from the session
+  const { getUser } = getKindeServerSession(req);
+  const user = await getUser();
+  if (!user || !user.id)
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
 
-      // Get the user object from the session
-      const { getUser } = getKindeServerSession(req, res);
-      const user = await getUser();
-      if (!user || user.id) return res.status(401).end('Unauthorized');
+  const { fileId, message } = schema.parse(await req.json());
+  const file = await api.file.get.query({ id: fileId });
+  if (!file)
+    return NextResponse.json({ error: 'File not found' }, { status: 404 });
 
-      const file = await prisma.file.findUnique({
-        where: { id: fileId, userId: user.id },
-      });
-      if (!file) return res.status(404).end('File not found');
+  // Create a new message in the database
+  const input: CreateMessageInput = {
+    text: message,
+    owner: !!user,
+    userId: user.id,
+    fileId: fileId,
+  };
+  const created = await api.message.create.mutate(input);
+  console.log('Message created', created.id);
 
-      // Create a new message object with the provided message text and the user's ID
-      const createdMessage = await prisma.message.create({
+  // Vectorize the message using the OpenAI embeddings
+  const embeddings = new OpenAIEmbeddings({
+    apiKey: env.OPENAI_API_KEY,
+  });
+
+  // Create a Pinecone index with the file's ID as the namespace
+  const pineconeIndex = pinecone.Index(env.PINECONE_INDEX);
+  const store = await PineconeStore.fromExistingIndex(embeddings, {
+    pineconeIndex,
+    namespace: file.id,
+  });
+
+  // Search for similar messages in the Pinecone index
+  const results = await store.similaritySearch(message, 5);
+
+  // Find the last 8 messages in the file
+  const messages = await prisma.message.findMany({
+    where: { fileId },
+    orderBy: { createdAt: 'asc' },
+    take: 8,
+  });
+
+  // Format the messages as an array of objects with 'role' and 'content' properties
+  const formattedMessages = messages.map((message) => ({
+    role: message.owner ? ('user' as const) : ('assistant' as const),
+    content: message.text,
+  }));
+
+  // Create a new OpenAI completion with the formatted messages and the context
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    temperature: 0,
+    stream: true,
+    messages: [
+      {
+        role: 'system',
+        content:
+          "Please use the provided context to answer the user's question in markdown format.",
+      },
+      {
+        role: 'user',
+        content: `Use the context and previous conversation below to answer the user's question in markdown format.
+          If unsure, state that you don't know instead of guessing.
+
+          ----------------
+
+          Previous Conversation:
+          ${formattedMessages
+            .map((message) => `${message.role}: ${message.content}`)
+            .join('\n')}
+
+          ----------------
+
+          Context:
+          ${results.map((result) => result.pageContent).join('\n\n')}
+
+          Prompt: ${message}`,
+      },
+    ],
+  });
+
+  // Create a new OpenAI stream with the completion
+  const stream = OpenAIStream(response, {
+    onCompletion: async (completion) => {
+      const _message = await prisma.message.create({
         data: {
           userId: user.id,
           fileId: fileId,
-          text: message,
-          owner: !!user,
+          text: completion,
+          owner: false,
         },
       });
-      console.log('Message created', createdMessage);
-
-      // Vectorize the message using the OpenAI embeddings
-      const embeddings = new OpenAIEmbeddings({
-        apiKey: env.OPENAI_API_KEY,
-      });
-
-      // Create a Pinecone index with the file's ID as the namespace
-      const pineconeIndex = pinecone.Index(env.PINECONE_INDEX);
-      const store = await PineconeStore.fromExistingIndex(embeddings, {
-        pineconeIndex,
-        namespace: file.id,
-      });
-
-      // Search for similar messages in the Pinecone index
-      const results = await store.similaritySearch(message, 5);
-
-      // Find the last 8 messages in the file
-      const messages = await prisma.message.findMany({
-        where: { fileId },
-        orderBy: { createdAt: 'asc' },
-        take: 8,
-      });
-
-      // Format the messages as an array of objects with 'role' and 'content' properties
-      const formattedMessages = messages.map((message) => ({
-        role: message.owner ? ('user' as const) : ('assistant' as const),
-        content: message.text,
-      }));
-
-      // Create a new OpenAI completion with the formatted messages and the context
-      const response = await openai.chat.completions.create({
-        model: 'gpt-3.5-turbo',
-        temperature: 0,
-        stream: true,
-        messages: [
-          {
-            role: 'system',
-            content:
-              "Please use the provided context to answer the user's question in markdown format.",
-          },
-          {
-            role: 'user',
-            content: `Use the context and previous conversation below to answer the user's question in markdown format. 
-              If unsure, state that you don't know instead of guessing.
-              
-              ----------------
-              
-              Previous Conversation:
-              ${formattedMessages
-                .map((message) => `${message.role}: ${message.content}`)
-                .join('\n')}
-              
-              ----------------
-              
-              Context:
-              ${results.map((result) => result.pageContent).join('\n\n')}
-              
-              Prompt: ${message}`,
-          },
-        ],
-      });
-
-      // Create a new OpenAI stream with the completion
-      const stream = OpenAIStream(response, {
-        onCompletion: async (completion) => {
-          const _message = await prisma.message.create({
-            data: {
-              userId: user.id,
-              fileId: fileId,
-              text: completion,
-              owner: false,
-            },
-          });
-          console.log('AI message created', _message);
-        },
-      });
-      return new StreamingTextResponse(stream);
-
-    default:
-      res.setHeader('Allow', ['POST']);
-      res.status(405).end(`Method ${method} Not Allowed`);
-  }
-}
+      console.log('AI message created', _message);
+    },
+  });
+  return new StreamingTextResponse(stream);
+};
